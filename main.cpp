ef441ab26fc940e826528bf41930ee8115f67071
Alex Nguyen <nguyenalex24@gmail.com>
2019-06-12 13:35:26 -0700

    Added copy constructor and destructor

--- main.cpp
+++ main.cpp
@@ -9,14 +9,16 @@ int main() {
 
 	std::cout << "There's a problem in Solitaire takeCard function\n\n\n\n";
 
-	std::cout << "Do you want to play Solitaire? (y/n): ";
+	BlackJack();
+
+	/*std::cout << "Do you want to play Solitaire? (y/n): ";
 	std::cin >> ans;
 	while (ans != "y" && ans != "Y" && ans != "n" && ans != "N" && ans != "yes" && ans != "Yes" && ans != "no" && ans != "No") {
 		std::cout << "Invalid response. Please enter y or n: ";
 		std::cin >> ans;
 	}
 	if (ans == "y" || ans == "Y" || ans == "yes" || ans == "Yes") Solitaire();
-
+*/
 
 	return 0;
 }
