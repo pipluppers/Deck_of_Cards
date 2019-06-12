ce525f1ad92c5a87d0730f6739e1884246cd0bd5
Alex Nguyen <nguyenalex24@gmail.com>
2019-06-12 13:46:12 -0700

    Added option for BlackJack

--- main.cpp
+++ main.cpp
@@ -5,20 +5,19 @@
 int main() {
 
 	srand( (unsigned) time(0));
-	std::string ans;
+	std::string action;
 
-	std::cout << "There's a problem in Solitaire takeCard function\n\n\n\n";
 
-	BlackJack();
-
-	/*std::cout << "Do you want to play Solitaire? (y/n): ";
-	std::cin >> ans;
-	while (ans != "y" && ans != "Y" && ans != "n" && ans != "N" && ans != "yes" && ans != "Yes" && ans != "no" && ans != "No") {
-		std::cout << "Invalid response. Please enter y or n: ";
-		std::cin >> ans;
+	std::cout << "What do you want to play?\n1. Solitaire\n2. BlackJack\n3. Quit";
+	std::cin >> action;
+	while (action != "1" && action != "2") {
+		std::cout << "Invalid response. Please enter \"1\" for Solitaire or \"2\" for BlackJack or \"3\" for Quit: ";
+		std::cin >> action;
+		std::cout << std::endl;
 	}
-	if (ans == "y" || ans == "Y" || ans == "yes" || ans == "Yes") Solitaire();
-*/
+	
+	if (action == "1") Solitaire();
+	else if (action == "2") BlackJack();
 
 	return 0;
 }
