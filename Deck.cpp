580fe8f6589df648f68dad9fe150822d804aaa57
Alex Nguyen <nguyenalex24@gmail.com>
2019-06-23 16:25:26 -0700

    Cleaned up

--- Deck.cpp
+++ Deck.cpp
@@ -31,7 +31,7 @@ std::ostream& operator<<(std::ostream& out, const Deck& d) {
 
 //	Default constructor
 Deck::Deck() {
-	int i = 0, j = 0;
+	int i = 0; Suit j = spade;
 	this->size = 52, this->current_size = 52;
 	deck = new Card[52];
 	for (; i < 13; ++i) {
@@ -76,7 +76,7 @@ Deck::~Deck() {
 Deck& Deck::operator=(const Deck& d) {
 	this->size = d.size;
 	this->current_size = d.current_size;
-	delete[] this->deck;
+	if (this->deck != 0) delete[] this->deck;
 	deck = new Card[this->size];
 	int i = 0;
 	for (i = 0; i < d.current_size; ++i) {
@@ -91,30 +91,19 @@ Deck& Deck::operator=(const Deck& d) {
 
 //	---------------	Functions	--------------------------------
 
-
-
-
-
 int Deck::currentSize() const {
 	return this->current_size;
 }
 
 void Deck::displayDeck() const {
-	std::cout << "----------\nDisplaying Deck\n";
+	std::cout << "----------" << std::endl <<  "Deck" << std::endl;
 	int i = 0;
 	for (; i < this->current_size; ++i) {
-		std::cout << i + 1 << ". " << this->deck[i];
+		std::cout << i + 1 << ". " << this->deck[i] << std::endl;
 	}
-	std::cout << "----------\n";
+	std::cout << "----------" << std::endl << std::endl;
 }
 
-/*
-	Input:
-		- Number of times to shuffle
-	Process:
-		- Pull random numbers a and b
-		- Move everything from a to b inclusive to the front
-*/
 void Deck::shuffle(int num) {
 	int begin, begin_temp, end, stop, i, j;
 	Card* tempDeck = new Card[this->current_size];
@@ -156,19 +145,13 @@ void Deck::shuffle(int num) {
 		- Index to check
 	Process:
 		- If index is out of range, exit
-
-		!!!!!!!!!!!!!!!!!
-		- IMPORTANT. INDEX MUST RANGE FROM 1 to CURRENT_SIZE, NOT 0 to CURRENT_SIZE - 1. THIS HELPS FOR DISPLAY FUNCTIONS !!!!!!
-		!!!!!!!!!!!!!!!!!
-
 		- Calling function should have a check for this default card
 	Output:
 		- Card
 */
 Card Deck::checkCard(int index) const {
-	if (index < 1 || index >= this->current_size + 1) std::cout << "Index is out-of-range\n";
-	assert(index >= 1 && index < this->current_size + 1);
-	--index;
+	if (index < 0 || index >= this->current_size) std::cout << "Index is out-of-range\n";
+	assert(index >= 0 && index < this->current_size);
 	return deck[index];
 }
 
@@ -176,82 +159,44 @@ Card Deck::checkCard(int index) const {
 	Input:
 		- Index to check
 	Process:
-		- If index is out of range, exit
-
-		!!!!!!!!!!!!!!!!!
-		- IMPORTANT. INDEX MUST RANGE FROM 1 to CURRENT_SIZE, NOT 0 to CURRENT_SIZE - 1. THIS HELPS FOR DISPLAY FUNCTIONS !!!!!!
-		!!!!!!!!!!!!!!!!!
-		
+		- If index is out of range, exit		
 		- Store card in temp variable and move everything over
 		- Decrease current size of deck
 	Output:
 		- Desired card
 */
 Card Deck::takeCard(int index) {
-	assert(index >= 1 && index < this->current_size + 1);
-	--index;
+	assert(index >= 0 && index < this->current_size);
 	Card temp = this->deck[index];
 	for (; index < this->current_size - 1; ++index) {
 		this->deck[index] = this->deck[index + 1];
 	}
 	--this->current_size;
-
 	return temp;
+}
+
+Card Deck::takeFrontCard() {
+	assert(this->current_size != 0);
+	return this->takeCard(0);
+}
+
+Card Deck::takeEndCard() {
+	assert(this->current_size != 0);
+	return this->takeCard(this->current_size - 1);
 }
 
 /*
 	Input:
 		- Card to add
+		- Location to add it before (e.g. if index = 0, add to front. if index = 2, add before the third card i.e. at index 2)
 	Process:
-		- Check if the deck is already full
-			- If yes: allocate space for the card and add it. Increment current_size and size
-			- If not: add card. Increment current_size
+		- Assert that the index is valid
+		- Check if deck is already full
+			- If yes, allocate space for the card and add it. Increment both current_size and size
+			- If no, add card. Increment only current_size
 */
-void Deck::addCardtoEnd(Card c) {
-	if (this->current_size == this->size) {
-		Card* tempDeck = new Card[this->size + 1];
-		int i = 0;
-		while (i < this->size) {
-			tempDeck[i] = this->deck[i];
-			++i;
-		}
-		tempDeck[i] = c;
-		delete[] this->deck;
-		this->deck = tempDeck;
-		++this->size;
-	}
-	else {
-		deck[this->current_size] = c;
-	}
-	++this->current_size;
-}
-
-void Deck::addCardtoFront(Card c) {
-	if (this->current_size == this->size) {
-		Card* tempDeck = new Card[this->size + 1];
-		int i = 1;
-		tempDeck[0] = c;
-		while (i < this->size + 1) {
-			tempDeck[i] = this->deck[i - 1];
-			++i;
-		}
-		delete[] this->deck;
-		this->deck = tempDeck;
-		++this->size;
-	}
-	else {
-		int i = this->current_size;
-		for (; i > 0; --i) {
-			this->deck[i] = this->deck[i - 1];
-		}
-		this->deck[i] = c;
-	}
-	++this->current_size;
-}
-
-void Deck::addCardtoLoc(Card c, int index) {
-	assert(index >= 1 && index <= current_size + 1);
-	--index;
+bool Deck::addCard(Card c, int index) {
+	if (index < 0 || index > this->current_size) return false;
 	if (this->current_size == this->size) {
 		Card* tempDeck = new Card[this->size + 1];
 		int i = 0;
@@ -259,13 +204,13 @@ void Deck::addCardtoLoc(Card c, int index) {
 			tempDeck[i] = this->deck[i];
 			++i;
 		}
-		tempDeck[i] = c;
+		tempDeck[i] = c;		// Shallow copy. Probably doesn't matter
 		++i;
 		while (i < this->size + 1) {
 			tempDeck[i] = this->deck[i - 1];
 			++i;
 		}
-		delete[] this->deck;
+		if (this->deck != 0) delete[] this->deck;
 		this->deck = tempDeck;
 		++this->size;
 	}
@@ -278,6 +223,15 @@ void Deck::addCardtoLoc(Card c, int index) {
 		this->deck[i] = c;
 	}
 	++this->current_size;
+	return true;
+}
+
+bool Deck::addCardtoEnd(Card c) {
+	return addCard(c, this->current_size);
+}
+
+bool Deck::addCardtoFront(Card c) {
+	return addCard(c, 0);
 }
 
 void Deck::fullempty() {
