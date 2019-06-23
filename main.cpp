5f0a25c6bc4cc65be6a76ddf1962d25bfc709815
Alex Nguyen <nguyenalex24@gmail.com>
2019-06-23 16:27:23 -0700

    Added newline after Quit option

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main() {
 	std::string action;
 
 
-	std::cout << "What do you want to play?\n1. Solitaire\n2. BlackJack\n3. Quit";
+	std::cout << "What do you want to play?\n1. Solitaire\n2. BlackJack\n3. Quit\n";
 	std::cin >> action;
 	while (action != "1" && action != "2") {
 		std::cout << "Invalid response. Please enter \"1\" for Solitaire or \"2\" for BlackJack or \"3\" for Quit: ";
