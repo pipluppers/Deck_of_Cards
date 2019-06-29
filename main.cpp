16a85d3d4ba0482a5c850c1a8c767554b1dcc6f6
Alex Nguyen <nguyenalex24@gmail.com>
2019-06-28 23:48:12 -0700

    Added Menu function for better readability

--- main.cpp
+++ main.cpp
@@ -1,23 +1,28 @@
+//	Property of Alex Nguyen (nguyenalex24@gmail.com)
+
 #include "Deck.h"
 #include <iostream>
 #include <ctime>
 
-int main() {
-
-	srand( (unsigned) time(0));
+void menu() {
 	std::string action;
-
-
-	std::cout << "What do you want to play?\n1. Solitaire\n2. BlackJack\n3. Quit\n";
+	std::cout << "What do you want to play?\n - 1. Solitaire\n - 2. BlackJack\n - 3. Quit\n";
 	std::cin >> action;
-	while (action != "1" && action != "2") {
+	while (action != "1" && action != "2" && action != "3") {
 		std::cout << "Invalid response. Please enter \"1\" for Solitaire or \"2\" for BlackJack or \"3\" for Quit: ";
 		std::cin >> action;
 		std::cout << std::endl;
 	}
-	
+
 	if (action == "1") Solitaire();
 	else if (action == "2") BlackJack();
+}
+
+int main() {
+
+	srand( (unsigned) time(0));
+
+	menu();
 
 	return 0;
 }
