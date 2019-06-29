da8fc72bb2fafe8645885f2dbf136813d8717061
Alex Nguyen <nguyenalex24@gmail.com>
2019-06-29 12:45:47 -0700

    Changed BlackJack to Blackjack

--- main.cpp
+++ main.cpp
@@ -6,16 +6,16 @@
 
 void menu() {
 	std::string action;
-	std::cout << "What do you want to play?\n - 1. Solitaire\n - 2. BlackJack\n - 3. Quit\n";
+	std::cout << "What do you want to play?\n - 1. Solitaire\n - 2. Blackjack\n - 3. Quit\n";
 	std::cin >> action;
 	while (action != "1" && action != "2" && action != "3") {
-		std::cout << "Invalid response. Please enter \"1\" for Solitaire or \"2\" for BlackJack or \"3\" for Quit: ";
+		std::cout << "Invalid response. Please enter \"1\" for Solitaire or \"2\" for Blackjack or \"3\" for Quit: ";
 		std::cin >> action;
 		std::cout << std::endl;
 	}
 
 	if (action == "1") Solitaire();
-	else if (action == "2") BlackJack();
+	else if (action == "2") Blackjack();
 }
 
 int main() {
